a08197a352c25e653f2cddaa5e431a2d5ced4912
bala <chanderbala13.bb@gmail.com>
2021-07-31 01:49:34 +0530

    initial commit

---
+++ todo/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import TodoForm
+from .models import Todo
+
+
+
+def index(request):
+
+	item_list = Todo.objects.order_by("-date")
+	if request.method == "POST":
+		form = TodoForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect('todo') 
+  
+	form = TodoForm()
+
+	page = {
+			"forms" : form,
+			"list" : item_list,
+			"title" : "TODO LIST",
+		}
+	return render(request,'todo/index.html', page)
+
+# function to remove item
+def remove(request,id):
+	item = Todo.objects.get(id=id)
+	item.delete()
+	messages.info(request, "Task deleted !!!")
+	return redirect('todo')
